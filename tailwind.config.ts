882f13f7923f877a05796736020d2543d5c3be68
raprayoga <prayogarafie@gmail.com>
2023-09-06 10:55:09 +0700

    config tailwind

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,24 @@
-import type { Config } from 'tailwindcss'
+import type { Config } from "tailwindcss";
 
 const config: Config = {
   content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      colors: {
+        primary: "#f13b2f",
+        red: "#f42619",
+        gray: "#c6c0c0",
+        "gray-1": "#ececec",
+        "gray-shadow": "#d1d1d1",
+        black: "#333130",
+        green: "#52bd94",
       },
     },
   },
   plugins: [],
-}
-export default config
+};
+export default config;
