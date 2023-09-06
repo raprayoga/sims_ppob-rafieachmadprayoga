073757997eea1302ea4f19480a35e005b0bbd27e
raprayoga <prayogarafie@gmail.com>
2023-09-06 12:54:45 +0700

    setup tailwind merge & cva

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,23 +1,33 @@
-import type { StorybookConfig } from '@storybook/nextjs';
+const path = require("path");
+import type { StorybookConfig } from "@storybook/nextjs";
 
 const config: StorybookConfig = {
-  stories: ['../src/**/*.mdx', '../src/**/*.stories.@(js|jsx|mjs|ts|tsx)'],
+  stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
   addons: [
-    '@storybook/addon-links',
-    '@storybook/addon-essentials',
-    '@storybook/addon-onboarding',
-    '@storybook/addon-interactions',
+    "@storybook/addon-links",
+    "@storybook/addon-essentials",
+    "@storybook/addon-onboarding",
+    "@storybook/addon-interactions",
     {
-      name: '@storybook/addon-styling',
+      name: "@storybook/addon-styling",
       options: {},
     },
   ],
   framework: {
-    name: '@storybook/nextjs',
+    name: "@storybook/nextjs",
     options: {},
   },
   docs: {
-    autodocs: 'tag',
+    autodocs: "tag",
+  },
+  webpackFinal: async (config, { configType }) => {
+    config.resolve.modules = [path.resolve(__dirname, ".."), "node_modules"];
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      "@": path.resolve(__dirname, "../src"),
+    };
+
+    return config;
   },
 };
 export default config;
